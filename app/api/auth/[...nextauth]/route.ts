12099905b08f5f397bbaf873f08a3f0c79098ca8
lukadfagundes <luka@sunny-stack.com>
2025-12-12 15:03:04 -0800

    Checkpoint

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -35,10 +35,20 @@ const config: NextAuthConfig = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
     }),
   ],
   session: {
     strategy: 'jwt',
+  },
+  pages: {
+    signIn: '/api/auth/signin',
   },
   callbacks: {
     async jwt({ token, user }) {
