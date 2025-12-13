74e845857dc1e76648b6c3ef5a290ae58cd72bd9
lukadfagundes <luka@sunny-stack.com>
2025-12-13 11:37:18 -0800

    Checkpoint

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -41,6 +41,18 @@ const config: NextAuthConfig = {
     strategy: 'jwt',
   },
   useSecureCookies: process.env.NODE_ENV === 'production',
+  cookies: {
+    pkceCodeVerifier: {
+      name: 'authjs.pkce.code_verifier',
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 60 * 15, // 15 minutes
+      },
+    },
+  },
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
